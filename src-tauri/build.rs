962c2df6466076aa47c0b2c07450daa5ea9e7388
WNLUO <admin@wnluo.com>
2025-12-04 21:19:02 +0800

    feat: 添加加密出口合规声明并更新版权信息

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -6,6 +6,9 @@ fn main() {
     // 设置本地化
     setup_localization();
 
+    // 设置加密出口合规声明
+    setup_encryption_compliance();
+
     // 设置 Tauri 构建配置
     tauri_build::build();
 }
@@ -40,7 +43,7 @@ fn setup_localization() {
 CFBundleName = "CaoGit";
 CFBundleDisplayName = "CaoGit";
 CFBundleGetInfoString = "CaoGit Git 管理器";
-NSHumanReadableCopyright = "Copyright © 2024 CaoGit. All rights reserved.";
+NSHumanReadableCopyright = "Copyright © 2024-2025 CaoGit. All rights reserved.";
 "#;
 
     let zh_strings = zh_cn_dir.join("InfoPlist.strings");
@@ -61,7 +64,7 @@ NSHumanReadableCopyright = "Copyright © 2024 CaoGit. All rights reserved.";
 CFBundleName = "CaoGit";
 CFBundleDisplayName = "CaoGit";
 CFBundleGetInfoString = "CaoGit Git Manager";
-NSHumanReadableCopyright = "Copyright © 2024 CaoGit. All rights reserved.";
+NSHumanReadableCopyright = "Copyright © 2024-2025 CaoGit. All rights reserved.";
 "#;
         fs::write(&en_strings, content).unwrap();
     }
@@ -69,4 +72,14 @@ NSHumanReadableCopyright = "Copyright © 2024 CaoGit. All rights reserved.";
     println!("cargo:rerun-if-changed=resources/zh_CN.lproj");
     println!("cargo:rerun-if-changed=resources/zh-Hans.lproj");
     println!("cargo:rerun-if-changed=resources/en.lproj");
+}
+
+fn setup_encryption_compliance() {
+    // 设置加密出口合规声明
+    // CaoGit 仅使用标准的 HTTPS 加密和 macOS Keychain，属于豁免范围
+    // 参考: https://developer.apple.com/documentation/security/complying_with_encryption_export_regulations
+
+    // 注意: Tauri 2.0 通过 tauri.conf.json 中的 bundle.macOS.info_plist 来配置
+    // 这里添加环境变量作为备用方案
+    println!("cargo:rustc-env=TAURI_MACOS_USES_NON_EXEMPT_ENCRYPTION=false");
 }
